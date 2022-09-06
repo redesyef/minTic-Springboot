60e79df1a6ab2b43be6ef45688447049b7a78de3
redesyef <ymauriciop@gmail.com>
2022-09-06 11:05:31 -0500

    service user

---
+++ src/main/java/mintic/segurity/SegurityApplication.java
@@ -0,0 +1,13 @@
+package mintic.segurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic/segurity/SegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.segurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
